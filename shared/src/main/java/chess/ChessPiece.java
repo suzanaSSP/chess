594be0df1b0c277e26c942004977e806c1eb51b8
suzanaSSP <suzanasp1026@gmail.com>
2026-01-12 21:11:58 -0700

    bishop moves, but doesn't defend yet

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,7 +11,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;       
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,7 +52,56 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            return bishopMoves(board, myPosition);
+        } else {    
+            return List.of();
+        }
     }
+
+    
+    public Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        List<ChessMove> bishop_moves = new java.util.ArrayList<>();
+        for (int i = 1; i <= 8; i++) {  
+            ChessMove move = new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), 
+            new ChessPosition(myPosition.getRow()+i, myPosition.getColumn()+i), null);
+            if (valid_position(board, move)) {
+                bishop_moves.add(move);
+            }
+
+            ChessMove move2 = new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), 
+            new ChessPosition(myPosition.getRow()-i, myPosition.getColumn()+i), null);
+            if (valid_position(board, move2)) {
+                bishop_moves.add(move2);
+            }
+
+            ChessMove move3 = new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), 
+                new ChessPosition(myPosition.getRow()+i, myPosition.getColumn()-i), null);
+            if (valid_position(board, move3)) {
+                bishop_moves.add(move3);
+            }
+            ChessMove move4 = new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), 
+                new ChessPosition(myPosition.getRow()-i, myPosition.getColumn()-i), null);
+                
+            if (valid_position(board, move4)) {
+                bishop_moves.add(move4);
+            }
+        }
+        return bishop_moves;
+    }
+
+    public Boolean valid_position(ChessBoard board, ChessMove move) {
+        ChessPosition position = move.getEndPosition();
+        if (position.getRow() >= 1 && position.getRow() <= 8 &&
+            position.getColumn() >= 1 && position.getColumn() <= 8 && 
+            board.getPiece(position) == null) {
+                return true;
+            } else {
+                return false;
+            }
+    }
+
 }
