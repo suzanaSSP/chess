529b62788d8702d59ac2ffaea077c721dc2e75b6
suzanaSSP <suzanasp1026@gmail.com>
2026-01-15 19:43:53 -0700

    created pawn file

--- shared/src/main/java/chess/PieceCalculators/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PieceCalculators/PawnMovesCalculator.java
@@ -1,4 +1,14 @@
 package chess.PieceCalculators;
 
-public class PawnMovesCalculator {
+import chess.ChessBoard;
+import chess.ChessPosition;
+
+public class PawnMovesCalculator extends PieceMoveCalculator{
+    public ChessPosition position;
+    public ChessBoard board;
+    public PawnMovesCalculator(ChessPosition current_position, ChessBoard board) {
+        super(current_position, board);
+        this.position = current_position;
+        this.board = board;
+    }
 }
